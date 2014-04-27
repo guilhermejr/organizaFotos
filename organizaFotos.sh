c7ec7283d14c0673bbe2cca18487bbac3af72fae
Guilherme Jr <falecom@guilhermejr.net>
2014-04-27 18:02:03 -0300

    Agora arquivos .png e .gif também são processados

--- organizaFotos.sh
+++ organizaFotos.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # --- Percorre todas as imagens e vídeos da pasta ---
-echo "$(find -E . -type f -iregex '.*\.(jpg|mov|mp4)$' -maxdepth 1 | cut -c3-)" | while read arquivo; do
+echo "$(find -E . -type f -iregex '.*\.(jpg|gif|png|mov|mp4)$' -maxdepth 1 | cut -c3-)" | while read arquivo; do
 
 	# --- Verifica se existe algum arquivo para processar ---
 	if [ ! -f "$arquivo" ]; then
